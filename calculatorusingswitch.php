863adf29be6a8c2bd039ad214c1a36778ce4493b
vmistry-21 <vmistry9896@gmail.com>
2026-03-15 19:18:58 +0530

    Create calculatorusingswitch.php

---
+++ calculatorusingswitch.php
@@ -0,0 +1,20 @@
+<?php
+$a = 10;
+$b = 5;
+$op = "+";
+
+switch($op){
+   case "+":
+       echo $a + $b;
+       break;
+   case "-":
+       echo $a - $b;
+       break;
+   case "*":
+       echo $a * $b;
+       break;
+   case "/":
+       echo $a / $b;
+       break;
+}
+?>
